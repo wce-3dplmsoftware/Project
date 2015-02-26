3181c68fd70fb554732b9d2a304fd945bdf5bf3e
wce-3dplmsoftware <rajat.rokade@walchandsangli.ac.in>
2015-02-26 11:26:15 +0530

    Update string.cpp

--- C-Assignments/Rajat-Rokade/string.cpp
+++ C-Assignments/Rajat-Rokade/string.cpp
@@ -1,10 +1,9 @@
 /*
-*A Program to implement string
+* A Program to implement string
 */
 
 #include "stdafx.h"
 #include <iostream>
-#include <cstring>
 
 using namespace std;
 
@@ -26,7 +25,7 @@ public:
 	Cstring(char *c ,int n)
 	{
 		str=new char[n];
-		str=c;
+		str=c+'\0';
 	}
 
 	Cstring(char *c)
@@ -35,35 +34,43 @@ public:
 	}
 
 	//string copy
-	void operator=(const Cstring &c)
+	void operator=(Cstring c)
 	{
-		str=c.str;
+	str=c.getStr();
 	}
 
 	//concatenation operator
-	void operator+=(const Cstring &c)
+	void operator+=(Cstring c)
 	{
-		char *a=(char*)malloc(strlen(str)+strlen(c.str)+1);
-		strcpy(a,str);
-		strcat(a,c.str);
-		str=a;
 		
+		char *a=(char*)malloc(size(str)+size(c.getStr())+1);
+		memcpy(a,str,size(str));
+		memcpy(a+(size(str)),c.getStr(),size(c.getStr()));
+		memcpy(a+size(str)+size(c.getStr()),"",size(a)-(size(str)+size(c.getStr())));
+		str=a;
 	}
 
 	//check equality of strings
-	bool operator==(const Cstring &c)
+	bool operator==(Cstring c)
 	{
-		if(str==c.str)
+		if(size(str)!=size(c.getStr()))
+			return false;
+		else
+		{
+			for(int i=0;i<size(str);i++)
+			{
+				if(str[i]!=c.getStr()[i])
+					return false;
+			}
 			return true;
-
-		return false;
+		}
 	}
 
 	//< operator to compare strings
-	bool operator<(const Cstring &c)
+	bool operator<(Cstring c)
 	{
 		int len=0;
-		for(int i=0;i<strlen(str);i++)
+		for(int i=0;i<size(str);i++)
 		{
 			if(str[i]=='\0')
 			{
@@ -72,20 +79,21 @@ public:
 			}
 		}
 
-		if(len<size(c))
+		if(len<size(c.getStr()))
 			return true;
 
 		return false;
 	}
 
 	//return size of string
-	int size(Cstring c)
+	int size(char *c)
 	{
-		for(int i=0;i<strlen(c.str);i++)
+		int i=0;
+		while(c[i]!='\0')
 		{
-			if(c.str[i]=='\0')
-				return i;
+			i++;
 		}
+		return i;
 	}
 
 	//[] to return character
@@ -101,6 +109,12 @@ public:
 		cout<<"String : "<<str<<endl;
 	}
 
+	//Get String
+	char* getStr()
+	{
+		return str;
+	}
+	
 };
 
 //main function
@@ -109,28 +123,28 @@ int main()
 	Cstring cstr;
 	Cstring cstr1(10);
 	Cstring cstr2("*",20);
-	Cstring cstr3("Computer");
+	Cstring cstr3("Computer Science");
 	Cstring cstr4;
 
 	//= operator
 	cstr4=cstr3;
 	cstr4.display();
-	
+
 	//concatenation operator
 	cstr2+=cstr3;
 	cstr2.display();
-	
+
 	//equality check operator
 	if(cstr4==cstr3)
 		cout<<"Equal Strings"<<endl;
-	
+
 	//relational operator
 	if(cstr2<cstr3)
 		cout<<"String 1 is smaller"<<endl;
 
 	//get character at given position
 	cout<<cstr3[3]<<endl;
-	
+
 	system("pause");
 	return 0;
 }
